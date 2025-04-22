02942571ca671f204148642875d585d6e25e0af2
Josh Scheitler <ja.scheitler03@gmail.com>
2025-04-21 21:58:36 -0500

    added calculator operations

--- src/calculator/calculatorModel.java
+++ src/calculator/calculatorModel.java
@@ -5,5 +5,48 @@ package calculator;
 //TODO - Implement Calculator Logic and state management
 
 public class calculatorModel {
+	
+	double memoryNumber;
+	
+	public calculatorModel() {
+		this.memoryNumber = 0.0;
+	}
+	
+	public double addition (double addends_a, double addends_b) {
+		double sum = addends_a + addends_b; 
+		return sum;
+	}
+	
+	public double subtraction (double minuend, double subtrahend) {
+		double difference = minuend - subtrahend;
+		return difference;
+	}
+	
+	public double multiplication (double multiplicand , double multiplier) {
+		double product = multiplicand * multiplier;
+		return product;
+	}
+	
+	public double division (double dividend, double divisor) {
+		double quotient = dividend / divisor;
+		return quotient;
+	}
+	
+	public double squareRoot (double radicand) {
+		double root = Math.sqrt(radicand);
+		return root;
+	}
+	
+	public double square(double num) {
+		double square = num * num;
+		return square;
+	}
+	
+	public void setMemory(double memNumber) {
+		this.memoryNumber = memNumber;
+	}
+	public double getMemory() {
+		return this.memoryNumber;
+	}
 
 }
